b31c14bc87b04014b53d38ea4cd2d10c6924ba80
yousuftarik <yousuftarik1@gmail.com>
2019-09-08 21:31:19 +0600

    admin setting complete

--- app/Providers/SettingServiceProvider.php
+++ app/Providers/SettingServiceProvider.php
@@ -1,26 +1,29 @@
 <?php
-
 namespace App\Providers;
-
+use Config;
+use App\Models\Setting;
+use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\ServiceProvider;
-
 class SettingServiceProvider extends ServiceProvider
 {
-   /**
-  * Register services.
-  *
-  * @return void
-  */
- public function register()
- {
-     $this->app->bind('settings', function ($app) {
-     return new Setting();
-     });
-     $loader = \Illuminate\Foundation\AliasLoader::getInstance();
-     $loader->alias('Setting', Setting::class);
- }
-
-   /**
+    /**
+     * @var bool
+     */
+    protected $defer = false;
+    /**
+     * Register services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->bind('settings', function ($app) {
+            return new Setting();
+        });
+        $loader = \Illuminate\Foundation\AliasLoader::getInstance();
+        $loader->alias('Setting', Setting::class);
+    }
+    /**
      * Bootstrap services.
      *
      * @return void
@@ -28,12 +31,12 @@ class SettingServiceProvider extends ServiceProvider
     public function boot()
     {
         // only use the Settings package if the Settings table is present in the database
-    if (!\App::runningInConsole() && count(Schema::getColumnListing('settings'))) {
-        $settings = Setting::all();
-        foreach ($settings as $key => $setting)
-        {
-            Config::set('settings.'.$setting->key, $setting->value);
+        if (!\App::runningInConsole() && count(Schema::getColumnListing('settings'))) {
+            $settings = Setting::all();
+            foreach ($settings as $key => $setting)
+            {
+                Config::set('settings.'.$setting->key, $setting->value);
+            }
         }
-    }
     }
 }
--- resources/views/admin/partials/sidebar.blade.php
+++ resources/views/admin/partials/sidebar.blade.php
@@ -13,23 +13,6 @@
                 <span class="app-menu__label">Dashboard</span>
             </a>
         </li>
-        <li class="treeview">
-            <a class="app-menu__item" href="#" data-toggle="treeview"><i class="app-menu__icon fa fa-users"></i>
-                <span class="app-menu__label">Users</span>
-                <i class="treeview-indicator fa fa-angle-right"></i>
-            </a>
-            <ul class="treeview-menu">
-                <li>
-                    <a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Admin Users</a>
-                </li>
-                <li>
-                    <a class="treeview-item" href="#" target="_blank" rel="noopener noreferrer"><i class="icon fa fa-circle-o"></i> Roles</a>
-                </li>
-                <li>
-                    <a class="treeview-item" href="#"><i class="icon fa fa-circle-o"></i> Permissions</a>
-                </li>
-            </ul>
-        </li>
         <li>
             <a class="app-menu__item {{ Route::currentRouteName() == 'admin.settings' ? 'active' : '' }}" href="{{ route('admin.settings') }}">
                 <i class="app-menu__icon fa fa-cogs"></i>
